699945d986dce5e1e5f9e1cd52a976119440877e
autom8ter <coleman.word@autom8ter.com>
2019-08-07 21:02:04 -0600

    client

--- azuread.go
+++ azuread.go
@@ -1,42 +1,45 @@
 package azure
 
 import (
+	"context"
 	"golang.org/x/oauth2"
+	"net/http"
 )
 
 var defaultScopes = []string{"User.Read"}
 
-func OAuth2Config(clientId string, redirect string, secret string, scopes ...string) *oauth2.Config {
-	if len(scopes) == 0 {
-		scopes = defaultScopes
-	}
-	return &oauth2.Config{
-		ClientID:     clientId,
-		ClientSecret: secret, // no client secret
-		RedirectURL:  redirect,
-
-		Endpoint: oauth2.Endpoint{
-			AuthURL:  "https://login.microsoftonline.com/common/oauth2/authorize",
-			TokenURL: "https://login.microsoftonline.com/common/oauth2/token",
-		},
-
-		Scopes: scopes,
-	}
+type Config struct {
+	ClientId string
+	Redirect string
+	Secret   string
+	Scopes   []string
 }
 
-func NewConfig(clientId string, redirect string, secret string, scopes ...string) *oauth2.Config {
-	if len(scopes) == 0 {
-		scopes = []string{"User.Read"}
+func (c *Config) Oauth2() *oauth2.Config {
+	if len(c.Scopes) == 0 {
+		c.Scopes = defaultScopes
 	}
 	cfg := &oauth2.Config{
-		ClientID:     clientId,
-		ClientSecret: secret,
+		ClientID:     c.ClientId,
+		ClientSecret: c.Secret,
 		Endpoint: oauth2.Endpoint{
 			AuthURL:  "https://login.microsoftonline.com/common/oauth2/authorize",
 			TokenURL: "https://login.microsoftonline.com/common/oauth2/token",
 		},
-		RedirectURL: redirect,
-		Scopes:      scopes,
+		RedirectURL: c.Redirect,
+		Scopes:      c.Scopes,
 	}
 	return cfg
+}
+
+func (c *Config) AuthCodeURL(state string, opts ...oauth2.AuthCodeOption) string {
+	return c.Oauth2().AuthCodeURL(state, opts...)
+}
+
+func (c *Config) Client(ctx context.Context, code string, opts ...oauth2.AuthCodeOption) (*http.Client, error) {
+	tok, err := c.Oauth2().Exchange(ctx, code, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return c.Oauth2().Client(ctx, tok), nil
 }
